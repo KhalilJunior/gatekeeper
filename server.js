21d5a745f08e04274d388237a19f25bc3930f355
KhalilJunior <51577045+KhalilJunior@users.noreply.github.com>
2019-06-11 20:44:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var { config, app } = require('./index');
 
-var port = process.env.PORT || config.port || 8000;
+var port = process.env.PORT || config.port || 8080;
 
 app.listen(port, null, function (err) {
   console.log('Gatekeeper, at your service: http://localhost:' + port);
